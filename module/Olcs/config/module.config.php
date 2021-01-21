8041dac1efcdecde276dba7bc0122daa2fa7a5b4
James Hodgson <james.hodgson@dvsa.gov.uk>
2021-01-21 10:34:45 +0000

    Revert VOL-1027
    This reverts commit 7313d5990b49709138fe30556fe5ab8d26a1e2ad.

--- module/Olcs/config/module.config.php
+++ module/Olcs/config/module.config.php
@@ -514,24 +514,15 @@ $routes = array(
                     ],
                 )
             ],
-            'process-response' => [
+            'process-response' => array(
                 'type' => Segment::class,
-                'options' => [
+                'options' => array(
                     'route' => '/process-response[/]',
-                    'defaults' => [
+                    'defaults' => array(
                         'action' => 'process-response',
-                    ],
-                ],
-            ],
-            'process-signature' => [
-                'type' => Segment::class,
-                'options' => [
-                    'route' => '/process-signature[/]',
-                    'defaults' => [
-                        'action' => 'process-signature',
-                    ],
-                ]
-            ],
+                    ),
+                )
+            )
         )
     ),
 );
@@ -1612,7 +1603,6 @@ return array(
                 'lva-application/transport_manager*' => ['selfserve-tm'],
                 'lva-variation/transport_manager*' => ['selfserve-tm'],
                 'lva-*' => ['selfserve-lva'],
-                'verify/process-response' => ['*'],
                 'search*' => ['*'],
                 'index' => ['*'],
                 'user-registration' => ['*'],
