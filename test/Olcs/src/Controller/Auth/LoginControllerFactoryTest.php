55e7f4e906180143333e5084f8814b1f8c5bc674
sanaullahkhan <sanaullah.khan@dvsa.gov.uk>
2024-01-15 13:45:34 +0000

    test(refactor): Removing dependencies on Common ServiceManage

--- test/Olcs/src/Controller/Auth/LoginControllerFactoryTest.php
+++ test/Olcs/src/Controller/Auth/LoginControllerFactoryTest.php
@@ -1,10 +1,8 @@
 <?php
-
 declare(strict_types=1);
 
 namespace OlcsTest\Controller\Auth;
 
-use Common\Auth\Adapter\CommandAdapter;
 use Common\Auth\Service\AuthenticationServiceInterface;
 use Common\Controller\Dispatcher;
 use Common\Controller\Plugin\CurrentUser;
@@ -13,32 +11,17 @@ use Common\Service\Helper\FormHelperService;
 use Dvsa\Olcs\Auth\Service\Auth\CookieService;
 use Laminas\Mvc\Controller\Plugin\FlashMessenger;
 use Laminas\Mvc\Controller\Plugin\Url;
-use Laminas\ServiceManager\ServiceManager;
-use Mockery\MockInterface;
 use Olcs\Auth\Adapter\SelfserveCommandAdapter;
 use Olcs\Controller\Auth\LoginController;
 use Olcs\Controller\Auth\LoginControllerFactory;
-use Mockery\Adapter\Phpunit\MockeryTestCase;
-use Common\Test\MocksServicesTrait;
-use Mockery as m;
-use Olcs\Form\Model\Form\Auth\Login;
+use PHPUnit\Framework\TestCase;
 
-class LoginControllerFactoryTest extends MockeryTestCase
+class LoginControllerFactoryTest extends TestCase
 {
-    //use MocksServicesTrait;
-
     /**
      * @var LoginControllerFactory
      */
     protected $sut;
-    private $serviceManager;
-
-    public function setUp(): void
-    {
-        $this->sut = m::mock(LoginControllerFactory::class)->makePartial();
-        $this->serviceManager = $this->createMockedServiceManager()
-;       $this->setUpDefaultServices($this->serviceManager);
-    }
 
     /**
      * @test
@@ -50,27 +33,6 @@ class LoginControllerFactoryTest extends MockeryTestCase
 
         // Assert
         $this->assertIsCallable([$this->sut, 'createService']);
-    }
-
-    /**
-     * @test
-     * @depends createService_IsCallable
-     * @depends __invoke_IsCallable
-     */
-    public function createService_CallsInvoke()
-    {
-        // Setup
-        $this->sut = m::mock(LoginControllerFactory::class)->makePartial();
-
-        // Expectations
-        $this->sut->expects('__invoke')->withArgs(function ($serviceManager, $requestedName) {
-            $this->assertSame($this->serviceManager, $serviceManager, 'Expected first argument to be the ServiceManager passed to createService');
-            $this->assertSame(Dispatcher::class, $requestedName, 'Expected requestedName to be NULL');
-            return true;
-        });
-
-        // Execute
-        $this->sut->createService($this->serviceManager);
     }
 
     /**
@@ -93,11 +55,21 @@ class LoginControllerFactoryTest extends MockeryTestCase
     {
         // Setup
         $this->setUpSut();
-        //$this->setUpDefaultServices();
-        $mockServiceManager = $this->createMockedServiceManager();
+        $serviceManager = $this->createMock(\Interop\Container\ContainerInterface::class);
+        $serviceManager->method('get')->willReturnMap([
+            [SelfserveCommandAdapter::class, $this->createMock(SelfserveCommandAdapter::class)],
+            [AuthenticationServiceInterface::class  , $this->createMock(AuthenticationServiceInterface::class)],
+            ['Auth\CookieService', $this->createMock(CookieService::class)],
+            [CurrentUser::class, $this->createMock(CurrentUser::class)],
+            [FlashMessenger::class, $this->createMock(FlashMessenger::class)],
+            [FormHelperService::class, $this->createMock(FormHelperService::class)],
+            [Redirect::class, $this->createMock(Redirect::class)],
+            [Url::class, $this->createMock(Url::class)],
+            ['ControllerPluginManager', $serviceManager]
+        ]);
 
         // Execute
-        $result = $this->sut->__invoke($mockServiceManager, null);
+        $result = $this->sut->__invoke($serviceManager, null);
 
         // Assert
         $this->assertInstanceOf(Dispatcher::class, $result);
@@ -107,73 +79,5 @@ class LoginControllerFactoryTest extends MockeryTestCase
     protected function setUpSut(): void
     {
         $this->sut = new LoginControllerFactory();
-    }
-
-    /**
-     * @param ServiceManager $serviceManager
-     */
-    protected function setUpDefaultServices(ServiceManager $serviceManager)
-    {
-        $serviceManager->setService(SelfserveCommandAdapter::class, $this->setUpMockService(SelfserveCommandAdapter::class));
-        $serviceManager->setService(AuthenticationServiceInterface::class, $this->setUpMockService(AuthenticationServiceInterface::class));
-        $serviceManager->setService('Auth\CookieService', $this->setUpMockService(CookieService::class));
-        $serviceManager->setService(CurrentUser::class, $this->setUpMockService(CurrentUser::class));
-        $serviceManager->setService(FlashMessenger::class, $this->setUpMockService(FlashMessenger::class));
-        $serviceManager->setService(FormHelperService::class, $this->setUpMockService(FormHelperService::class));
-        $serviceManager->setService(Redirect::class, $this->setUpMockService(Redirect::class));
-        $serviceManager->setService(Url::class, $this->setUpMockService(Url::class));
-    }
-
-    private function createMockedServiceManager()
-    {
-        $mockedServiceManager = $this->getMockBuilder(ServiceManager::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        // Set up Expectation for method calls
-        $mockedServiceManager->expects($this->any())
-            ->method('get')
-            ->willReturnCallback([$this, 'getService']);
-
-        return $mockedServiceManager;
-    }
-    public function getService($serviceName)
-    {
-            // Return the mocked services based on the service name
-        switch ($serviceName) {
-            case SelfserveCommandAdapter::class:
-                return $this->setUpMockService(SelfserveCommandAdapter::class);
-            case AuthenticationServiceInterface::class:
-                return $this->setUpMockService(AuthenticationServiceInterface::class);
-            case 'Auth\CookieService':
-                return $this->setUpMockService(CookieService::class);
-            case CurrentUser::class:
-                return $this->setUpMockService(CurrentUser::class);
-            case FlashMessenger::class:
-                return $this->setUpMockService(FlashMessenger::class);
-            case FormHelperService::class:
-                return $this->setUpMockService(FormHelperService::class);
-            case Redirect::class:
-                return $this->setUpMockService(Redirect::class);
-            case Url::class:
-                return $this->setUpMockService(Url::class);
-            case CommandAdapter::class:
-                return $this->setUpMockService(CommandAdapter::class);
-
-            default:
-                return null;
-        }
-    }
-
-
-    /**
-     * @param string $class
-     * @return MockInterface
-     */
-    protected function setUpMockService(string $class): MockInterface
-    {
-        $instance = m::mock($class);
-        $instance->shouldIgnoreMissing();
-        return $instance;
     }
 }
