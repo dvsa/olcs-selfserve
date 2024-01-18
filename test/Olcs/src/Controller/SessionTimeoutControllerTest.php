30e2cf36c53c8d47d6734d4c6ac0e0f9c36324a3
sanaullahkhan <sanaullah.khan@dvsa.gov.uk>
2024-01-18 12:56:31 +0000

    test(refactor): Change to ContainerInterface from ServiceManager for SessionTimeoutControllerTest

--- test/Olcs/src/Controller/SessionTimeoutControllerTest.php
+++ test/Olcs/src/Controller/SessionTimeoutControllerTest.php
@@ -1,5 +1,6 @@
 <?php
-declare(strict_types = 1);
+
+declare(strict_types=1);
 
 namespace OlcsTest\Controller;
 
@@ -7,19 +8,14 @@ use Common\Controller\Plugin\Redirect;
 use Common\Rbac\JWTIdentityProvider;
 use Common\Rbac\User;
 use Interop\Container\Containerinterface;
-use Laminas\ServiceManager\ServiceManager;
 use Laminas\Http\Request;
 use Laminas\Http\Response;
-use Laminas\Mvc\Controller\PluginManager;
 use Laminas\Mvc\MvcEvent;
-use Laminas\Router\Http\TreeRouteStack;
 use Laminas\Router\RouteMatch;
-use Laminas\ServiceManager\ServiceLocatorInterface;
 use Laminas\Stdlib\Parameters;
 use Laminas\Uri\Http;
 use Laminas\View\Model\ViewModel;
 use Mockery as m;
-use Mockery\MockInterface;
 use Olcs\Controller\SessionTimeoutController;
 use Olcs\Controller\SessionTimeoutControllerFactory;
 use LmcRbacMvc\Identity\IdentityProviderInterface;
@@ -32,11 +28,7 @@ use PHPUnit\Framework\TestCase;
  */
 class SessionTimeoutControllerTest extends TestCase
 {
-
     protected const COOKIE_NAME = 'cookie';
-
-    private $identityProviderClass = JWTIdentityProvider::class;
-
     /**
      * @test
      */
@@ -50,7 +42,6 @@ class SessionTimeoutControllerTest extends TestCase
 
     /**
      * @test
-     * @depends indexAction_IsCallable
      */
     public function indexAction_ReturnsViewModelIfIdentityIsAnonymous()
     {
@@ -99,21 +90,23 @@ class SessionTimeoutControllerTest extends TestCase
 
     /**
      * @test
-     * @depends indexAction_ReturnsViewModelIfIdentityIsAnonymous
      */
     public function indexAction_ReturnsViewModelIfIdentityIsNull()
     {
-        // Setup
-        $serviceLocator = $this->setUpServiceLocator();
-        $sut = $this->setUpSut($serviceLocator, new Request());
+        // Mock the IdentityProviderInterface
+        $identityProvider = $this->createMock(IdentityProviderInterface::class);
+        $identityProvider->expects($this->once())
+            ->method('getIdentity')
+            ->willReturn(null);
 
-        // Define Expectations
-        $currentUser = $this->resolveMockService($serviceLocator, IdentityProviderInterface::class);
-        $currentUser->shouldReceive('getIdentity')->withNoArgs()->andReturnNull()->once();
+        // Mock dependencies
+        $redirectHelper = $this->createMock(Redirect::class);
+
+        // Instantiate the controller manually, injecting the mocked dependencies
+        $controller = new SessionTimeoutController($identityProvider, $redirectHelper);
 
         // Execute
-        $result = $sut->indexAction($this->setUpRequest());
-
+        $result = $controller->indexAction($this->setUpRequest());
         // Assert
         $this->assertInstanceOf(ViewModel::class, $result);
     }
@@ -121,97 +114,55 @@ class SessionTimeoutControllerTest extends TestCase
 
     /**
      * @test
-     * @depends indexAction_ReturnsViewModelIfIdentityIsNull
      */
     public function indexAction_LogsOutUserIfLoggedIn()
     {
-        // Setup
-        $serviceLocator = $this->setUpServiceLocator();
-        $request = $this->setUpRequest();
-        $sut = $this->setUpSut($serviceLocator, new Request());
+        // Mock JWTIdentityProvider
+        $identityProvider = $this->createMock(JWTIdentityProvider::class);
+        // Assume the identity is not anonymous (logged in)
+        $identityProvider->expects($this->once())
+            ->method('getIdentity')
+            ->willReturn($this->createMock(User::class));
+        // Mock the clearSession method
+        $identityProvider->expects($this->once())
+            ->method('clearSession');
 
-        $this->setUpIdentityWithClearSession($this->identityProviderClass);
+        // Assume refresh() returns a Response object
+        $redirectHelper = $this->setUpRedirect();
+
+        // Instantiate the controller manually, injecting the mocked dependencies
+        $controller = new SessionTimeoutController($identityProvider, $redirectHelper);
 
         // Execute
-        $response = $sut->indexAction($request);
+        $result = $controller->indexAction($this->createMock(Request::class));
 
         // Assert
-        $this->assertInstanceOf(Response::class, $response);
+        $this->assertInstanceOf(Response::class, $result);
     }
 
     /**
      * @test
-     * @depends indexAction_LogsOutUserIfLoggedIn
-     * @dataProvider dpIdentityProviderClass
      */
-    public function indexAction_RedirectsUserIfLoggedIn(string $identityProviderClass)
+    public function indexAction_RedirectsUserIfLoggedIn()
     {
-        // Setup
-        $serviceLocator = $this->setUpServiceLocator();
-        $request = $this->setUpRequest();
-        $sut = $this->setUpSut($serviceLocator, new Request());
+        // Mock JWTIdentityProvider
+        $identityProvider = $this->createMock(JWTIdentityProvider::class);
+        // Assume the identity is not anonymous (logged in)
+        $identityProvider->expects($this->once())
+        ->method('getIdentity')
+        ->willReturn($this->createMock(User::class));
 
-        $this->setUpIdentityWithClearSession($identityProviderClass);
+        // Mock Redirect plugin
+        $redirectHelper = $this->setUpRedirect();
 
-        // Define Expectations
-        $redirectHelper = $this->resolveMockService($serviceLocator, Redirect::class);
-        $redirectHelper->shouldReceive('refresh')
-            ->withNoArgs()
-            ->andReturn($expectedResponse = new Response())
-            ->once();
+        // Instantiate the controller manually, injecting the mocked dependencies
+        $controller = new SessionTimeoutController($identityProvider, $redirectHelper);
 
         // Execute
-        $response = $sut->indexAction($request);
+        $response = $controller->indexAction($this->createMock(Request::class));
 
         // Assert
-        $this->assertSame($expectedResponse, $response);
-    }
-
-    public function dpIdentityProviderClass(): array
-    {
-        return [
-            [$this->identityProviderClass],
-            [JWTIdentityProvider::class],
-        ];
-    }
-
-    /**
-     * "
-     * @param ServiceLocatorInterface $serviceLocator
-     * @return array
-     */
-    protected function setUpDefaultServices(ServiceLocatorInterface $serviceLocator): array
-    {
-        return [
-            IdentityProviderInterface::class => $this->setUpIdentity($this->identityProviderClass),
-            Redirect::class => $this->setUpRedirect(),
-            'request' => $this->setUpMockService(Request::class),
-        ];
-    }
-
-    /**
-     * @param Request $request
-     * @param RouteMatch $routeMatch
-     * @return MvcEvent
-     */
-    protected function setUpMvcEvent(Request $request, RouteMatch $routeMatch): MvcEvent
-    {
-        $event = new MvcEvent();
-        $event->setRequest($request);
-        $event->setRouteMatch($routeMatch);
-        $router = $this->setUpMockService(TreeRouteStack::class);
-        $event->setRouter($router);
-        return $event;
-    }
-
-    /**
-     * @param ServiceLocatorInterface $serviceLocator
-     * @return PluginManager
-     */
-    protected function setUpPluginManager(Containerinterface $serviceLocator): PluginManager
-    {
-        $pluginManager = new PluginManager($serviceLocator);
-        return $pluginManager;
+        $this->assertInstanceOf(Response::class, $response);
     }
 
     /**
@@ -222,12 +173,6 @@ class SessionTimeoutControllerTest extends TestCase
         // Mock Dependencies
         $identityProvide = $this->createMock(IdentityProviderInterface::class);
         $redirectHelper = $this->createMock(Redirect::class);
-        $cookieService = $this->createMock(CookieService::class);
-        $logoutService = $this->createMock(LogoutService::class);
-
-        //Mock Event
-        $event = $this->createMock(MvcEvent::class);
-        $pluginManager = $this->createMock(PluginManager::class);
 
         return new SessionTimeoutController(
             $identityProvide,
@@ -254,65 +199,15 @@ class SessionTimeoutControllerTest extends TestCase
     }
 
     /**
-     * @param string $identityProvider
-     * @return m\MockInterface
+     * @return Redirect
      */
-    protected function setUpIdentity(string $identityProvider): m\MockInterface
+    protected function setUpRedirect(): Redirect
     {
-        $identity = $this->setUpMockService(User::class);
-        $identity->shouldReceive('isAnonymous')
-            ->andReturnFalse()
-        ->byDefault();
-
-        $currentUser =  $this->getMockServiceWithName($identityProvider, IdentityProviderInterface::class);
-        $currentUser->shouldReceive('getIdentity')
-            ->withNoArgs()
-            ->andReturn($identity)
-            ->byDefault();
-        $currentUser->expects('clearSession')
-            ->never()
-            ->byDefault();
-
-        return $currentUser;
-    }
-
-    protected function getMockServiceWithName(string $class, string $serviceName): MockInterface
-    {
-        if (!$this->serviceManager->has($serviceName)) {
-            $this->serviceManager->setService(
-                $serviceName,
-                $this->setUpMockService($class)
-            );
-        }
-
-        return $this->serviceManager->get($serviceName);
-    }
-
-    protected function setUpIdentityWithClearSession(string $identityProvider): void
-    {
-        $identity = $this->setUpMockService(User::class);
-        $identity->expects('isAnonymous')
-            ->withNoArgs()
-            ->andReturnFalse();
-
-        $currentUser =  $this->getMockServiceWithName($identityProvider, IdentityProviderInterface::class);
-        $currentUser->expects('getIdentity')
-            ->withNoArgs()
-            ->andReturn($identity);
-        $currentUser->expects('clearSession')
-            ->withNoArgs();
-    }
-
-    /**
-     * @return m\MockInterface
-     */
-    protected function setUpRedirect(): m\MockInterface
-    {
-        $redirect = $this->setUpMockService(Redirect::class);
-        $redirect->shouldReceive('refresh')
-            ->withNoArgs()
-            ->andReturn(new Response())
-            ->byDefault();
+        // Mock Redirect plugin
+        $redirect = $this->createMock(Redirect::class);
+        $redirect->expects($this->once())
+            ->method('refresh')
+            ->willReturn(new Response());
 
         return $redirect;
     }
