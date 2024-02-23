b7a7a7073b1cb3899e87e650bd1ae11c51eb9c07
JoshuaLicense <JoshuaLicense@users.noreply.github.com>
2024-02-23 10:06:08 +0000

    feat: migrate config
    fix: add config tweaks from Laminas
    
    !fixup

---
+++ config/autoload/config-parameter.global.php
@@ -0,0 +1,29 @@
+<?php
+
+use Dvsa\LaminasConfigCloudParameters\Cast\Boolean;
+use Dvsa\LaminasConfigCloudParameters\ParameterProvider\Aws\SecretsManager;
+use Dvsa\LaminasConfigCloudParameters\ParameterProvider\Aws\ParameterStore;
+
+$environment = getenv('ENVIRONMENT_NAME') ?: 'reg';
+
+$providers = [];
+
+if (!empty($environment)) {
+    $providers = [
+        SecretsManager::class => [
+            sprintf('DEVAPP%s-BASE-SM-APPLICATION-SELFSERVE', strtoupper($environment)),
+        ],
+        ParameterStore::class => [
+            sprintf('/applicationparams/%s/', strtolower($environment)),
+        ],
+    ];
+}
+
+return [
+    'config_parameters' => [
+        'providers' => $providers,
+    ],
+    'casts' => [
+        '[query_cache][enabled]' => Boolean::class,
+    ],
+];
